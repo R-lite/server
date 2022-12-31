ed05d3a36c2db799abee1d09df324536f68f43a0
R-lite <muminraj15@gmail.com>
2022-12-31 05:30:43 +0100

    Completed backend

--- index.js
+++ index.js
@@ -14,9 +14,12 @@ import postRoutes from './routes/posts.js';
 import { createPost } from './controllers/posts.js';
 import { register } from './controllers/auth.js'
 import { verifyToken } from "./middleware/auth.js";
+import User from "./models/User.js";
+import Post from "./models/Post.js";
+import { users, posts } from "./data/index.js";
 
 
-/* COFIG */
+/* CONFIG */
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 dotenv.config();
@@ -43,7 +46,7 @@ const upload = multer({ storage });
 
 /* ROUTES WITH FILES UPLOAD */
 app.post("/auth/register", upload.single("picture"), register); 
-app.post("/posts", verifyToken. upload.single("picture"), createPost);
+app.post("/posts", verifyToken, upload.single("picture"), createPost);
 
 /* ROUTES */
 app.use('/auth', authRoutes);
@@ -64,4 +67,9 @@ mongoose.connect(process.env.MONGO_URL, {
 })
 .then(() => {
     app.listen(PORT, () => console.log(`Sever port: ${PORT}`));
-}).catch((err) => console.log(`${err} did not connect`))
+    
+    /* POPULATE DB WIRH DUMMY USERS */
+    // User.insertMany(users);
+    // Post.insertMany(posts); 
+}
+).catch((err) => console.log(`${err} did not connect`))
--- routes/posts.js
+++ routes/posts.js
@@ -3,12 +3,16 @@ import {
     getFeedPosts,
     getUserPosts,
     likePost
-} from "../controllers/posts";
+} from "../controllers/posts.js";
 import { verifyToken } from "../middleware/auth.js"
 
 const router = express.Router();
 
-
+/* READ */
 router.get('/', verifyToken, getFeedPosts);
 router.get('/:userId/posts', verifyToken, getUserPosts);
 
+/* UPDATE */
+router.patch("/:id/like", verifyToken, likePost);
+
+export default router;
